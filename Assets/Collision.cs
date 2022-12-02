621db22cde1afe6077074a02c4b289564569787d
Willywonka443 <spaceiskool1998@gmail.com>
2022-12-01 21:51:25 -0500

    Added all kinds of stuff

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -1,17 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+   
+    [SerializeField] Color hasPackageColor = new Color(1,1,1,1);
     
+    [SerializeField] Color noPackageColor = new Color(1,1,1,1);
+    
+    SpriteRenderer spriteRenderer;
+    bool hasPackage = false;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void  OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("OUCH!!!");
+        
+        if(other.gameObject.tag == "Enemy"){
+            Debug.Log("Car Crashed"); 
+            SceneManager.LoadScene("SampleScene");
+        } else{
+            Debug.Log("OUCH!!!");
+        }
     }
     
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Congrats!!1");
+       
+       if(other.gameObject.tag == "Package" && !hasPackage){
+            Debug.Log("Picked up package");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, 0.25f);
+       } else if(other.gameObject.tag == "Customer" && hasPackage){
+            Debug.Log("Package Delivered");
+            hasPackage = false;
+             spriteRenderer.color = noPackageColor;
+            
+       }
+        
     }
+   
         
     
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,21 +5,40 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.099f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float currentSpeed = 0.01f;
+    
+    [SerializeField] float boostSpeed = 0.03f;
+
+    [SerializeField] float bumpSpeed  = 0.01f;
+    float startSpeed;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        startSpeed = currentSpeed;
     }
+    private void OnTriggerEnter2D(Collider2D other) {
+      
+       if(other.tag == "Boost"){
+            Debug.Log("I have my zoomies");
+            currentSpeed = boostSpeed;
+            // Destroy(other.gameObject, 0.25f);
+       }
 
+       if(other.tag == "Bumps"){
+            Debug.Log("You shall Slow Down");
+            currentSpeed = bumpSpeed;
+       }
+
+    }
     // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * Time.deltaTime;
         float speedAmount = Input.GetAxis("Vertical") * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount * steerSpeed);
-        transform.Translate(0,speedAmount * moveSpeed,0);
+        transform.Translate(0,speedAmount * currentSpeed,0);
     }
 }
  
